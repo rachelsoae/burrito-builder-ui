25644cb43a0877961d48cae44d2b348a0920feeb
Rachel Prather <rachelprather1@gmail.com>
2023-08-07 08:47:28 -0700

    Add keys to mapped elements

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -4,7 +4,7 @@ import "./Orders.css";
 const Orders = ({orders}) => {
   const orderEls = orders.map((order) => {
     return (
-      <div className="order">
+      <div key={order.id} className="order">
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map((ingredient) => {
