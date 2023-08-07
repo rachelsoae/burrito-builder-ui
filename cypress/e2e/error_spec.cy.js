c15d71cc0ae2b54431f1ee32d19baa4054d75789
Rachel Prather <rachelprather1@gmail.com>
2023-08-07 09:49:39 -0700

    Test form sad path handling

---
+++ cypress/e2e/error_spec.cy.js
@@ -0,0 +1,37 @@
+describe("Form Error Handling", () => {
+  beforeEach(() => {
+    cy.loadPage();
+  })
+  
+  it("should display an alert if form is submitted blank", () => {
+    cy.wait('@getOrders').then((interception) => {
+      cy.get('button').contains('Submit Order').click()
+      cy.on('window:alert', (alert) => {
+        expect(alert).to.contains('Please fill out the order form.')
+      })
+    })
+  });
+
+  it("should display an alert if form is submitted without a name", () => {
+    cy.wait('@getOrders').then((interception) => {
+      cy.get('button').contains('beans').click()
+      cy.get('button').contains('sofritas').click()
+      cy.get('button').contains('queso fresco').click()
+      cy.get('button').contains('jalapenos').click()
+      cy.get('button').contains('Submit Order').click()
+      cy.on('window:alert', (alert) => {
+        expect(alert).to.contains('Please enter a name for this order.')
+      })
+    })
+  });
+
+  it("should display an alert if form is submitted without ingredients", () => {
+    cy.wait('@getOrders').then((interception) => {
+      cy.get('input[type=text]').type('Rachel');
+      cy.get('button').contains('Submit Order').click()
+      cy.on('window:alert', (alert) => {
+        expect(alert).to.contains('Please select at least one ingredient.')
+      })
+    })
+  });
+});
