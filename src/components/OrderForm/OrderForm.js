abed55560d3949ea981d4ac40d1028e633aa5188
Rachel Prather <rachelprather1@gmail.com>
2023-08-07 11:02:38 -0700

    Start cost calculation functionality

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -5,10 +5,12 @@ import './OrderForm.css'
 const OrderForm = ({setOrders}) => {
   const [name, setName] = useState("");
   const [ingredients, setIngredients] = useState([]);
+  const [cost, setCost] = useState(0);
 
   const handleClick = (e) => {
     e.preventDefault();
-    ingredients.includes(e.target.name) ? alert(`You already added ${e.target.name} to your burrito!`) : setIngredients(prevState => [...prevState, e.target.name])    
+    ingredients.includes(e.target.name) ? alert(`You already added ${e.target.name} to your burrito!`) : setIngredients(prevState => [...prevState, e.target.name])
+    calculateCost()  
   }
 
   const handleSubmit = (e) => {
@@ -19,6 +21,17 @@ const OrderForm = ({setOrders}) => {
       .then(response => setOrders(prevState => [...prevState, response]))
       clearInputs();
     }
+  }
+
+  const calculateCost = () => {
+    console.log(ingredients)
+    const cost = ingredients.reduce((currentIngredient, totalCost) => {
+      console.log(currentIngredient)
+          totalCost += possibleIngredients[currentIngredient].cost
+        return totalCost;
+    }, 0)
+    setCost(cost)
+    console.log(cost)
   }
 
   const checkForm = () => {
@@ -39,27 +52,63 @@ const OrderForm = ({setOrders}) => {
   };
 
   const possibleIngredients = [
-    "beans",
-    "steak",
-    "carnitas",
-    "sofritas",
-    "lettuce",
-    "queso fresco",
-    "pico de gallo",
-    "hot sauce",
-    "guacamole",
-    "jalapenos",
-    "cilantro",
-    "sour cream",
+    {
+      name: 'beans',
+      cost: 1.00
+    },
+    {
+      name: 'steak',
+      cost: 3.50
+    },
+    {
+      name: 'carnitas',
+      cost: 3.50
+    },
+    {
+      name: 'sofritas',
+      cost: 3.00
+    },
+    {
+      name: 'lettuce',
+      cost: 0.50
+    },
+    {
+      name: 'queso fresco',
+      cost: 1.50
+    },
+    {
+      name: 'pico de gallo',
+      cost: 1.50
+    },
+    {
+      name: 'hot sauce',
+      cost: 1.00
+    },
+    {
+      name: 'guacamole',
+      cost: 2.50
+    },
+    {
+      name: 'jalapenos',
+      cost: 1.00
+    },
+    {
+      name: 'cilantro',
+      cost: 0.50
+    },
+    {
+      name: 'sour cream',
+      cost: 1.00
+    }
   ];
   const ingredientButtons = possibleIngredients.map((ingredient) => {
     return (
       <button
-        key={ingredient}
-        name={ingredient}
+        key={ingredient.name}
+        name={ingredient.name}
         onClick={(e) => handleClick(e)}
       >
-        {ingredient}
+        {ingredient.name} - ${ingredient.cost.toFixed(2)}
       </button>
     );
   });
@@ -80,7 +129,7 @@ const OrderForm = ({setOrders}) => {
 
       <p className='order-summary'>Order: {ingredients.join(", ") || "Nothing selected"}</p>
 
-      <button onClick={(e) => handleSubmit(e)} className='submit'>Submit Order</button>
+      <button onClick={(e) => handleSubmit(e)} className='submit'>Submit Order ${cost || '0.00'}</button>
     </form>
   );
 }
