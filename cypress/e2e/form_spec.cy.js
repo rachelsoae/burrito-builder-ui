fc73474e809d0946dae1ca38e79f5dbddba176d6
Rachel Prather <rachelprather1@gmail.com>
2023-08-07 09:36:53 -0700

    Test form submission and display update

---
+++ cypress/e2e/form_spec.cy.js
@@ -0,0 +1,30 @@
+describe("Page Load Behavior", () => {
+  it("should submit and display a new order", () => {
+    cy.loadPage();
+    cy.postOrder();
+
+    cy.wait('@getOrders').then((interception) => {
+      cy.get('input[type=text]').type('Rachel');
+      cy.get('button').contains('beans').click()
+        .get('p').contains('beans');
+      cy.get('button').contains('sofritas').click()
+        .get('p').contains('beans, sofritas');
+      cy.get('button').contains('queso fresco').click()
+        .get('p').contains('beans, sofritas, queso fresco');
+      cy.get('button').contains('jalapenos').click()
+        .get('p').contains('beans, sofritas, queso fresco, jalapenos');
+      cy.get('button').contains('Submit Order').click()
+
+
+      cy.wait('@postOrder').then((interception) => {
+        
+        cy.get('section').children().should('have.length', 4)
+          .get('section').children().last().contains('h3', 'Rachel')
+          .get('section').children().last().contains('li', 'beans')
+          .get('section').children().last().contains('li', 'sofritas')
+          .get('section').children().last().contains('li', 'queso fresco')
+          .get('section').children().last().contains('li', 'jalapenos')
+      })
+    })
+  });
+});
